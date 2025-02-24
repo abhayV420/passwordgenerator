28e1cfec09ce99d5ca18ff97fa1dee21a4540681
abhayV420e <abhayV420@outlook.com>
2025-02-24 19:55:00 +0530

    Made some UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -39,11 +39,11 @@ function App() {
 
   return (
   <div className='bg-black w-full h-screen flex justify-center py-11 text-orange-400'>
-     <div className='bg-gray-800 h-[15%] w-[90%] sm:w-[35%] rounded-lg flex flex-col py-4 px-5 gap-8 min-w-[550px] '>
+     <div className='bg-gray-800 h-[17%] w-[90%] sm:w-[35%] rounded-lg flex flex-col py-4 px-5 gap-8 min-w-[550px] '>
 
                         {/* upper scetion */}
             <div className='flex gap-3'>
-              <input className='w-[87%] h-11 rounded-lg ' value={password} type="text" readOnly ref={passRef} />
+              <input className='w-[87%] h-11 rounded-lg pl-2' value={password} type="text" readOnly ref={passRef} />
               <button className='bg-blue-600 text-white rounded-lg w-[100px] hover:bg-blue-800' onClick={savePassToClipBoard} >Copy</button>
             </div>
 
@@ -51,7 +51,7 @@ function App() {
 
                <div className='flex gap-3 mx-6 '>
                 
-                 <div className='flex text-xl gap-1'>
+                 <div className='flex text-lg gap-1'>
                   <input className='cursor-pointer' id='range' type="range" min={6} max={30} value={length} onChange={(e)=>{setLength(e.target.value)}} />
                   <label htmlFor='range' >Length:{length}</label>
                  </div>
